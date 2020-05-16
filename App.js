a92ece55e3682ebc77a9d90c37b6e32b56a6a23b
Pinkowo <40238691+Pinkowo@users.noreply.github.com>
2020-05-16 19:47:51 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { Text, View, StyleSheet,Image } from 'react-native';
+import { Text, View, StyleSheet,Image,AsyncStorage } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { SplashScreen } from 'expo';
 
 import Mask from './screen/Mask';
 import Pharmacy from './screen/Pharmacy';
@@ -9,28 +10,58 @@ import News from './screen/News';
 import pics from "./json/pics.json";
 
 const Tab = createMaterialTopTabNavigator();
+const PERSISTENCE_KEY = "ALBUMS_NAVIGATION_STATE";
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <View style={styles.header}>
-        <Image source={{uri: pics.profile}} style={styles.profile}/>
-      </View>
-      <Tab.Navigator
-        tabBarOptions={{
-          activeTintColor: '#B36D69',
-          labelStyle: { fontSize: 20 },
-          indicatorStyle :{
-            backgroundColor:'#B36D69'
-          }    
-        }}
+  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+  const [initialNavigationState, setInitialNavigationState] = React.useState();
+  React.useEffect(() => {
+    async function loadResourcesAndDataAsync() {
+      try {
+        SplashScreen.preventAutoHide();
+        const savedStateString = await AsyncStorage.getItem(PERSISTENCE_KEY);
+        const state = JSON.parse(savedStateString);
+        setInitialNavigationState(state);
+      } catch (e) {
+        // We might want to provide this error information to an error reporting service
+        console.warn(e);
+      } finally {
+        setLoadingComplete(true);
+        SplashScreen.hide();
+      }
+    }
+    loadResourcesAndDataAsync();
+  }, []);
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <NavigationContainer
+        initialState={initialNavigationState}
+        onStateChange={(state)=>
+          AsyncStorage.setItem(PERSISTENCE_KEY, JSON.stringify(state))
+        }
       >
-        <Tab.Screen name="口罩" component={Mask} />
-        <Tab.Screen name="藥局" component={Pharmacy} />
-        <Tab.Screen name="消息" component={News} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+        <View style={styles.header}>
+          <Image source={{uri: pics.profile}} style={styles.profile}/>
+        </View>
+        <Tab.Navigator
+          tabBarOptions={{
+            activeTintColor: '#B36D69',
+            labelStyle: { fontSize: 20 },
+            indicatorStyle :{
+              backgroundColor:'#B36D69'
+            }    
+          }}
+        >
+          <Tab.Screen name="口罩" component={Mask} />
+          <Tab.Screen name="藥局" component={Pharmacy} />
+          <Tab.Screen name="消息" component={News} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
